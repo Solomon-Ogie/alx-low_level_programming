8435f735e15ade99306401e8c6a26714fb2c82c8
Solomon Eragbie <seragbie@gmail.com>
2022-09-12 09:58:48 -0700

    Print_alphabt

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -8,17 +8,14 @@
  */
 int main(void)
 {
-	char low, e, q;
-
-	e = 'e';
-	q = 'q';
-
-	for (low = 'a'; low <= 'z'; low++)
+	char ch;
+	for (ch = 'a' ; ch <= 'z' ; ch++)
 	{
-		if (low != e && low ! = q)
-			putchar(low);
+		if (ch != 'e' && ch != 'q')
+		{
+			putchar(ch);
+		}
 	}
-	printf("\n");
-
+	putchar('\n');
 	return (0);
 }
