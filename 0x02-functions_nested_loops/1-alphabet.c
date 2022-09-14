b627dd45540c37339d52da988560d3a0aab4c05e
Solomon Eragbie <seragbie@gmail.com>
2022-09-14 16:39:39 -0700

    Alphbet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,6 +3,7 @@
  * main - Start Point
  *print_alphabet - code to print alphabet
  *
+ * Description - print_alphabet - function to print abcd
  * Return: 0 (Success)
  */
 void print_alphabet(void)
