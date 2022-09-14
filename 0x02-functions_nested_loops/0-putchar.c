84df1bb05ab7f5ad1042fc4e1ab921cb34bd8ac6
Solomon Eragbie <seragbie@gmail.com>
2022-09-14 15:29:08 -0700

    Putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Start Point
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	printf("_putchar\n');
+
+	return (0);
+}
