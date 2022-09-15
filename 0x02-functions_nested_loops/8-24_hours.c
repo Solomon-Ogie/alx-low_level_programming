1fc02660f8147038ec2a8aae4d4c32861f209f81
Solomon Eragbie <seragbie@gmail.com>
2022-09-14 23:52:36 -0700

    24_hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -7,25 +7,17 @@
  */
 void jack_bauer(void)
 {
-	int hours = 0;
-	int minutes = 0;
-	int hours_remainder;
-	int mins_remainder;
-
-	while (hours <= 23)
+	int h, m;
+	for (h = 0; h < 24; h++)
 	{
-		while (minutes <= 59)
+		for (m = 0; m < 60; m++)
 		{
-			av
-				hours_remainder = hours % 10;
-			_putchar(hours / 10 + '0');
-			_putchar(hours_remainder + '0')
-				_putchar(':');
-			_putchar(minutes / 10 + '0');
-			minutes++
-				_putchar('\n');
-		}
-		hours++;
-		minutes = 0;
-	}
-}
+			_putchar((h / 10 + '0');
+					_putchar((h % 10 + '0');
+						_putchar(':');
+						_putchar((m / 10 + '0');
+							_putchar((m % 10 + '0');
+								_putchar('\n');
+								}
+								}
+								}
