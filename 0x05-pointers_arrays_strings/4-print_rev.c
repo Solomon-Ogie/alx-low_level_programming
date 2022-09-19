86d917bd86857354e6648f180e55e956dfb15fbe
Solomon Eragbie <seragbie@gmail.com>
2022-09-19 04:23:42 -0700

    Task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string in reverse order
+ * @s: string to revers
+ *
+ * Return: null
+ */
+
+void print_rev(char *s)
+{
+	int len = strlen(s);
+
+	while (len--)
+		putchar(*s(s + len));
+	putchar(10);
+}
