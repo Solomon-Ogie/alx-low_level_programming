07f0ca9223c34a2f4b91564b9291423d70313fcc
Solomon Eragbie <seragbie@gmail.com>
2022-09-19 21:18:44 -0700

    Task 11

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "main.h"
+
+/**
+ * keygen - a program that generates random valid passwords
+ * for the program 101.crackme
+ *
+ * Return: generated password
+ */
+
+int main(void)
+{
+	char c;
+	int x;
+
+	srand(time(0));
+	while (x <= 2645)
+	{
+		c = rand() % 128;
+		x += c;
+		putchar(c);
+	}
+	putchar(2772 - x);
+
+	return (0);
+}
