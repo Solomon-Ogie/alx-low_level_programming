c6b05c637a8cb0c0ed20ad0f4d0e38e9b7bb1a61
Solomon Eragbie <seragbie@gmail.com>
2022-09-21 16:44:54 -0700

    Task 11

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -3,20 +3,47 @@
 /**
  * print_number - Prints an integer.
  * @n: the integer to be printed.
+ *
+ * Return: 0
  */
 
 void print_number(int n)
 {
-	unsigned int num = n;
+	int power, peg, riff;
 
+	peg = 0;
+	power = 1;
+	riff = n;
 	if (n < 0)
 	{
 		_putchar('-');
-		num = -num
+		peg = 1;
 	}
 
-	if ((num / 10) > 0)
-		print_number(num / 10);
+	while (riff > 9 || hold < -9)
+	{
+		power *= 10;
+		riff /= 10;
+	}
 
-	_putchar((num % 10) + '0');
+	while (power > 0)
+	{
+		if (power > 9)
+		{
+			if (:peg)
+				_putchar((n / power % 10) + '0');
+			else
+				_putchar((n / power % 10) * -1 + '0');
+
+			power /= 10;
+		}
+		if (power == 1)
+		{
+			if (peg)
+				_putchar((n % 10) * -1 + '0');
+			else
+				_putchar(n % 10 + '0');
+			power = 0;
+		}
+	}
 }
