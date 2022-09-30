1c8605508760abfd5ce724d753195f74dd7beb7c
Solomon Eragbie <seragbie@gmail.com>
2022-09-30 10:55:19 -0700

    Task 3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -3,11 +3,10 @@
 #include <stdio.h>
 
 /**
- * main - prints the multiplication of two integers
- * @argc: Counts the argument
- * @argv: argument vector
- *
- * Return: 0 (true) 1 (false)
+ *main - prints the multiplication of two integers
+ *@argc: Counts the argument
+ *@argv: argument*
+ *Return: 0 if true, 1 if false
  */
 
 int main(int argc, char *argv[])
