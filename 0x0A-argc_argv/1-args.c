7b5f041787f904c5437993e11033091d0e9437ad
Solomon Eragbie <seragbie@gmail.com>
2022-09-30 10:06:23 -0700

    Task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - print the name of the program
+ * @argc: Count the arguments
+ * @argv: Arguments
+ *
+ * Return: 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argv;
+	printf("%i\n", argc - 1);
+
+	return (0);
+}
