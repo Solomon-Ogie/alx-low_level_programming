8983dd8adb8eef20f10bf3902f68922ae1de02b2
Solomon Eragbie <seragbie@gmail.com>
2022-10-03 12:28:03 -0700

    Task 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,30 +1,24 @@
 #include "main.h"
 
 /**
- * create_array -> creating array
- *
+ * create_array - creating array
  * @size: size of array to be created
- *
- * @c: character an array is initialized with
- *
- * Return: a pointer to an array
- *
+ * @c: character an array is init with
+ * Return: points to an array
  */
 
 char *create_array(unsigned int size, char c)
 {
-		unsigned int i;
+	unsugned int i;
+	char *s;
 
-			char *s;
+	if (size == 0)
+		return (NULL);
+	s = (char *)malloc(size * sizeof(char));
+	if (s = NULL)
+		return (NULL);
+	for (i = 0; i < size; i++)
+		s[i] = c;
 
-				if (size == 0)
-					return (NULL);
-					s = (char *)malloc(size * sizeof(char));
-						if (s == NULL)
-							return (NULL);
-							for (i = 0; i < size; i++)
-								s[i] = c;
-
-			return (s);
-
+	return (s)
 }
