d8e6c5ed03ab4affff8e68fb133365456b520307
Solomon Eragbie <seragbie@gmail.com>
2022-10-03 11:15:50 -0700

    Task 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -17,24 +17,14 @@ char *create_array(unsigned int size, char c)
 
 			char *s;
 
-
-
 				if (size == 0)
-
-							return (NULL);
-
+					return (NULL);
 					s = (char *)malloc(size * sizeof(char));
-
 						if (s == NULL)
-
-									return (NULL);
-
+							return (NULL);
 							for (i = 0; i < size; i++)
+								s[i] = c;
 
-										s[i] = c;
-
-
-
-								return (s);
+			return (s);
 
 }
