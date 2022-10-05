4416539beef7e5c2d718e30d4c80c3d0ef3875e1
Solomon Eragbie <seragbie@gmail.com>
2022-10-05 01:42:22 -0700

    Task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,42 +1,58 @@
 #include "main.h"
+/**
+ * _strlen - counts the array
+ * @s: array of elements
+ * Return: 1
+ */
+
+int _strlen(char *s)
+{
+	unsigned int i;;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	return (i);
+}
 
 /**
- * str_concat - string concatenating function
- * @s1: string 1
- * @s2: string 2
- * Return: string 1 + string 2
+ * str_concat - points to array
+ * @s1: first array
+ * @s2: second array
+ * Return: an array dynamic
  */
 
 char *str_concat(char *s1, char *s2)
 {
-	int i = 0, j = 0, l = 0, k = 0;
-	char *s;
+	char *dst;
+	unsigned int i, j, size;
 
 	if (s1 == NULL)
 		s1 = "";
+
 	if (s2 == NULL)
 		s2 = "";
-	while (s1[i])
-		i++;
-	while (s2[j])
-		j++;
 
-	l = i + j;
-	s = (char *)malloc(l * sizeof(char) + 1);
-	if (s == NULL)
-		return (NULL);
-	j = 0;
-	while (k < 1)
+	size = (_strlen(s1) + _strlen(s2) + 1);
+
+	dst = (char *) malloc(size * sizeof(char));
+
+	if (dst == 0)
 	{
-		if (k < i)
-			s[k] = s1[k];
-		if (k >= i)
-		{
-			s[k] = s2[k];
-			j++;
-		}
-		k++;
+		return (NULL);
 	}
-	s[k] = '\0';
-	return (s);
+
+	for (i = 0; *(s1 + i) != '\0'; i++)
+		*(dst + i) = *(s1 + i);
+
+	for (j = 0; *(s2 + j) != '\0'; j++)
+	{
+		*(dst + i) = *(s2 + j);
+		i++;
+	}
+
+	return (dst);
 }
