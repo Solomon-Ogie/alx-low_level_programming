7ee569649e1a6e2f8573e781dc0ecaabba67947d
Solomon Eragbie <seragbie@gmail.com>
2022-10-13 10:20:33 -0700

    Task 3

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -49,7 +49,6 @@ void print_all(const char * const format, ...)
 				}
 				printf("%s", str);
 				break;
-		}i++;
-	}
-	printf("\n"), va_end(valist);
+		} i++;
+	} printf("\n"), va_end(valist);
 }
