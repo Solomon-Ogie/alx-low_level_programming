29abb5da4f569d668cbd3bde10ce0292e0f8c762
Solomon Eragbie <seragbie@gmail.com>
2022-10-21 08:41:58 -0700

    Task 3

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -16,7 +16,7 @@ list_t *add_node_end(list_t **head, const char *str)
 	list_t *new, *last;
 
 	new = malloc(sizeof(list_t));
-	if (new_node == NULL)
+	if (new == NULL)
 		return (NULL);
 
 	dup = strdup(str);
